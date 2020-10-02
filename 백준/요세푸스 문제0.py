420e8b234114bbc9c603bdd1725e08acd3762e21
Sangwoo Suk <56511253+sangwoo24@users.noreply.github.com>
2020-10-02 16:23:20 +0900

    백준 자료구조 문제풀이

--- 백준/요세푸스 문제0.py
+++ 백준/요세푸스 문제0.py
@@ -1,6 +1,5 @@
-from collections import deque as d
 import sys
-'''
+
 n,k = map(int,sys.stdin.readline().split())
 
 lst = [i+1 for i in range(n)]
@@ -23,20 +22,5 @@ while lst:
         cnt = 0
     
 print("<" + ', '.join(ans) + ">")
-'''
 
-n, k = map(int,sys.stdin.readline().split())
-lst = d([i+1 for i in range(n)])
-ans = []
-cnt = 0
-
-while lst:
-    if cnt < k-1:
-        lst.rotate(-1)
-        cnt += 1
-    else:
-        ans.append(str(lst.popleft()))
-        cnt = 0
-
-print("<" + ', '.join(ans) + ">")
 
